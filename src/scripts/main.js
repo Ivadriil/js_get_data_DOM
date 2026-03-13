2d4bfb7fe222b33d40cf618ad61a7a7350cc9145
Ivadriil <petrivi51@gmail.com>
2026-03-13 16:01:15 +0200

    add task solution2

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,24 +1,18 @@
 'use strict';
 
-const populaition = document.getElementsByClassName('population');
-
-console.log(populaition);
-
-function populationNumber() {
-    let allPeople = 0;
-    for (let i = 0; i < populaition.length; i++) {
-        allPeople += parseInt((populaition[i].textContent).split(',').join(''));
-    }
-    return allPeople;
-};
-
-console.log(populationNumber(populaition)); 
-
+const population = document.getElementsByClassName('population');
 const totalPopulation = document.getElementsByClassName('total-population');
-
-totalPopulation[0].textContent = populationNumber(populaition).toLocaleString(); 
-
 const averagePopulation = document.getElementsByClassName('average-population');
 
-averagePopulation[0].textContent = Math.round(populationNumber(populaition) / populaition.length).toLocaleString();
+function populationNumber() {
+  let allPeople = 0;
 
+  for (let i = 0; i < population.length; i++) {
+    allPeople += parseInt(population[i].textContent.split(',').join(''));
+  }
+
+  totalPopulation[0].textContent = allPeople.toLocaleString();
+  averagePopulation[0].textContent = Math.round(allPeople / population.length).toLocaleString();
+}
+
+populationNumber(population);
