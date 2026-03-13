3c708d0d903ebe5c787f4ff5b0990ee166858a08
Ivadriil <petrivi51@gmail.com>
2026-03-13 15:43:41 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,24 @@
 'use strict';
 
-// write your code here
+const populaition = document.getElementsByClassName('population');
+
+console.log(populaition);
+
+function populationNumber() {
+    let allPeople = 0;
+    for (let i = 0; i < populaition.length; i++) {
+        allPeople += parseInt((populaition[i].textContent).split(',').join(''));
+    }
+    return allPeople;
+};
+
+console.log(populationNumber(populaition)); 
+
+const totalPopulation = document.getElementsByClassName('total-population');
+
+totalPopulation[0].textContent = populationNumber(populaition).toLocaleString(); 
+
+const averagePopulation = document.getElementsByClassName('average-population');
+
+averagePopulation[0].textContent = Math.round(populationNumber(populaition) / populaition.length).toLocaleString();
+
